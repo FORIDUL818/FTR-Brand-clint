1bd2ca39130614e4893d1a12855d05f2fb90f682
Foridul <foridulislamdise880@gmail.com>
2024-10-03 18:22:43 +0600

    just practice okey

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@
 function App() {
   return (
     <div>
-      <h1 className=" bg-slate-600">hi my name is foridul islam</h1>
+      <h1 className=" bg-slate-600">hi  my name is foridul islam</h1>
+      <h1>samp vary samp bro</h1>
     </div>
   )
 }
